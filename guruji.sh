51f54a11eff1d79eb9daec6c94731cf75a79be27
soodkunal <ksood315@gmail.com>
2025-08-29 18:16:31 +0530

    update

--- guruji.sh
+++ guruji.sh
@@ -734,10 +734,6 @@ function g8_step1_get_from_prof() {
     echo "--------------------------------------------------"
     echo "Please make sure your metadata follows the above format."
     echo ""
-
-    read -p "Enter Professor ID: " G8_PROF_ID
-    # read -p "Enter Professor Name: " G8_PROF_NAME
-
     read -p "Enter URL to download the lecture text file: " G8_TXT
     [ -z "$G8_TXT" ] && echo " ! Error: No URL provided for text." && return 1
 
@@ -791,8 +787,8 @@ function g8_step1_get_from_prof() {
     fi
     
      # Export so other steps can use
-    export G8_IN_PROF_TXT="$G8_PROJ_DIR/input/g8_in_${G8_PROF_ID}_${G8_PROF_NAME}_${TIMESTAMP}.txt"
-    export G8_IN_MTDT="$G8_PROJ_DIR/input/g8_in_${G8_PROF_ID}_${G8_PROF_NAME}_${TIMESTAMP}.json"
+    export G8_IN_PROF_TXT="$G8_PROJ_DIR/input/g8_in_${prof_id}_${G8_PROF_NAME}_${TIMESTAMP}.txt"
+    export G8_IN_MTDT="$G8_PROJ_DIR/input/g8_in_${prof_id}_${G8_PROF_NAME}_${TIMESTAMP}.json"
 }
 
 function take_professor_entry_interactive() {
